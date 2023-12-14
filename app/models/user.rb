58df98d76642b6da85064d20fa1a70e80a0de4a4
Esteban <cepalacios@outlook.com>
2023-12-13 20:04:30 -0500

    Implement last 3 posts method in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   has_many :posts, foreign_key: :author_id
+
+  scope :last_three_posts, lambda { |user_id|
+    Post.where(author_id: user_id).order('posts.created_at DESC').limit(3)
+  }
 end
