2dc7bbe26c402bc1646a26f599d36a2bdb8b357d
Esteban <cepalacios@outlook.com>
2023-12-14 09:23:18 -0500

    Add has_many comments and likes association to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_many :posts, foreign_key: :author_id
+  has_many :comments
+  has_many :likes
 
   scope :last_three_posts, lambda { |user_id|
     Post.where(author_id: user_id).order('posts.created_at DESC').limit(3)
