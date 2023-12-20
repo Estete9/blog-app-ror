7c46a28971b803afd69b8fcaedf7b9ec57394816
Esteban <cepalacios@outlook.com>
2023-12-20 10:39:19 -0500

    Implement status, template, and placeholder text tests for PostsController

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,9 +1,7 @@
 require 'rails_helper'
 
-# require 'pry'
-
 RSpec.describe 'Posts', type: :request do
-  let(:valid_attributes) do
+  let(:user_valid_attributes) do
     {
       name: 'Tom',
       photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
@@ -12,14 +10,52 @@ RSpec.describe 'Posts', type: :request do
     }
   end
 
-  describe 'GET /posts' do
+  describe 'GET /posts/index' do
+    before do
+      @user = User.create! user_valid_attributes
+      get user_posts_path(user_id: @user.id)
+    end
+
     it 'response is successful' do
-      user = User.create! valid_attributes
-      get user_posts_path(user_id: user.id)
-
-      # binding.pry
-
       expect(response).to be_successful
+    end
+
+    it 'response renders the correct page' do
+      expect(response.body).to render_template('index')
+    end
+
+    it 'contains the correct text in the body' do
+      expect(response.body).to include('Post #1')
+    end
+  end
+
+  describe 'GET /posts/show' do
+    before do
+      @user = User.create! user_valid_attributes
+      @post = Post.create! post_valid_attributes
+      get user_post_path(user_id: @user.id, id: @post.id)
+    end
+
+    let(:post_valid_attributes) do
+      {
+        author: @user,
+        title: 'hello',
+        text: 'This is my first post',
+        likes_counter: 0,
+        comments_counter: 0
+      }
+    end
+
+    it 'response is successful' do
+      expect(response).to be_successful
+    end
+
+    it 'response renders the correct page' do
+      expect(response.body).to render_template('show')
+    end
+
+    it 'contains the correct text in the body' do
+      expect(response.body).to include('Post #1')
     end
   end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -17,4 +17,33 @@ RSpec.describe 'Users', type: :request do
       expect(response.body).to include('User Name')
     end
   end
+
+  describe 'GET /users/show' do
+    let(:valid_attributes) do
+      {
+        name: 'Tom',
+        photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+        bio: 'Teacher from Mexico.',
+        posts_counter: 0
+      }
+    end
+
+    let(:user) { User.create! valid_attributes }
+
+    before do
+      get user_path(id: user.id)
+    end
+
+    it 'response is successful' do
+      expect(response).to be_successful
+    end
+
+    it 'response renders the correct page' do
+      expect(response.body).to render_template('show')
+    end
+
+    it 'contains the correct text in the body' do
+      expect(response.body).to include('Bio')
+    end
+  end
 end
