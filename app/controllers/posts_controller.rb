aeba9ce846d4c39556cc0bd6fccf7c691ea212a0
Esteban <cepalacios@outlook.com>
2023-12-21 19:58:39 -0500

    Implement new_post_form partial with the create action in the PostsController

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,18 @@ class PostsController < ApplicationController
     @user = User.find(params[:user_id])
     @post = @user.posts.find(params[:id])
   end
+
+  def create
+    @user = current_user
+    @post = Post.new(post_params)
+    return unless @post.save
+
+    redirect_to user_posts_path(@user.id)
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
   root to: 'users#index'
 
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+    resources :posts, only: [:index, :show, :create]
+  end
+
+  resources :posts, only: [] do
+    resources :comments, only: [:create]
+    resources :likes, only: [:create]
   end
 end
