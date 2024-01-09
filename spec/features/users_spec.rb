a1c578f68a3be914657a00d32d01011dc1385a27
Jose RM <mojica.raul@gmail.com>
2024-01-08 18:00:35 -0600

    Add testing to see the profile picture for each user.

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.feature "Users", type: :feature do
   let(:create_users) do
-    @user1 = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.', posts_counter: 0)
-    @user2 = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from USA.', posts_counter: 0)
+    @user1 = User.create(name: 'Tom', photo: 'https://picsum.photos/100', bio: 'Teacher from Mexico.', posts_counter: 0)
+    @user2 = User.create(name: 'Lilly', photo: 'https://picsum.photos/100', bio: 'Teacher from USA.', posts_counter: 0)
   end
 
   scenario "displays a list with the users' names" do
@@ -13,6 +13,16 @@ RSpec.feature "Users", type: :feature do
 
     [@user1, @user2].each do |user|
       expect(page).to have_content(user.name)
+    end
+  end
+
+  scenario "displays a list with the users' photos" do
+    create_users
+
+    visit users_path
+
+    [@user1, @user2].each do |user|
+      expect(page).to have_css("img[src*='#{user.photo}']")
     end
   end
 end
