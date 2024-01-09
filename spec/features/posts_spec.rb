58849e1404b37eece94305a6de022e6c084ebb70
Esteban <cepalacios@outlook.com>
2024-01-09 16:36:31 -0500

    Add post title test to posts#show

--- spec/features/posts_spec.rb
+++ spec/features/posts_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-RSpec.feature "Posts", type: :feature do
-  context "posts#index page" do
+RSpec.feature 'Posts', type: :feature do
+  context 'posts#index page' do
     let(:create_objects) do
       @user1 = User.create(name: 'Tom', photo: 'https://picsum.photos/100', bio: 'Teacher from Mexico.', posts_counter: 0)
       @post1 = Post.create(author: @user1, title: 'first', text: 'This is my first post', comments_counter: 0, likes_counter: 3)
@@ -63,6 +63,24 @@ RSpec.feature "Posts", type: :feature do
       post_element.click
 
       expect(page).to have_current_path(user_post_path(@post1.author_id, @post1.id))
+    end
+  end
+
+  context 'posts#show page' do
+    let(:create_objects) do
+      @user1 = User.create(name: 'Tom', photo: 'https://picsum.photos/100', bio: 'Teacher from Mexico.', posts_counter: 0)
+      @post1 = Post.create(author: @user1, title: 'first', text: 'This is my first post', comments_counter: 0, likes_counter: 3)
+      @comment1 = Comment.create(post: @post1, user: @user1, text: 'first! Hi, I\'m Tom!' )
+      @comment2 = Comment.create(post: @post1, user: @user1, text: 'second! Hi I\'m also Tom!' )
+    end
+
+    before do
+      create_objects
+      visit user_post_path(@post1.author_id, @post1.id)
+    end
+
+    scenario "I can see the post's title" do
+      expect(page).to have_content('first')
     end
   end
 end
