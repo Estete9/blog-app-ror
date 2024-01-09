82f8e73b21eab3e16d539f72289bf378b536a4c2
Esteban <cepalacios@outlook.com>
2024-01-09 15:23:12 -0500

    Add profile picture test to posts#index

---
+++ spec/features/posts_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.feature "Posts", type: :feature do
+  context "posts#index page" do
+    let(:create_objects) do
+      @user1 = User.create(name: 'Tom', photo: 'https://picsum.photos/100', bio: 'Teacher from Mexico.', posts_counter: 0)
+      @post1 = Post.create(author: @user1, title: 'first', text: 'This is my first post', comments_counter: 0, likes_counter: 0)
+      @post2 = Post.create(author: @user1, title: 'second', text: 'This is my second post', comments_counter: 0, likes_counter: 0)
+      @post3 = Post.create(author: @user1, title: 'third', text: 'This is my third post', comments_counter: 0, likes_counter: 0)
+      @post4 = Post.create(author: @user1, title: 'fourth', text: 'This is my fourth post', comments_counter: 0, likes_counter: 0)
+      @post5 = Post.create(author: @user1, title: 'fifth', text: 'This is my fifth post', comments_counter: 0, likes_counter: 0)
+      @post6 = Post.create(author: @user1, title: 'sixth', text: 'This is my sixth post', comments_counter: 0, likes_counter: 0)
+      @post7 = Post.create(author: @user1, title: 'seventh', text: 'This is my seventh post', comments_counter: 0, likes_counter: 0)
+    end
+
+    before do
+      create_objects
+      visit user_posts_path(@user1.id)
+    end
+
+    scenario "I can see the user's profile picture" do
+      expect(page).to have_css("img[src*='#{@user1.photo}']")
+    end
+  end
+
+end
