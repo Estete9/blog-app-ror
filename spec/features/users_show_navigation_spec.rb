8e16a058411d185b5fbd4e4bb7b168421c1cf255
Esteban <cepalacios@outlook.com>
2024-01-10 08:07:13 -0500

    add test for see all posts button users_show_navigation_spec

--- spec/features/users_show_navigation_spec.rb
+++ spec/features/users_show_navigation_spec.rb
@@ -2,35 +2,43 @@
 
 require 'rails_helper'
 
+require 'pry'
+
 RSpec.feature 'UsersShows Navigation', type: :feature do
   context 'users#show page navigating to related pages' do
-    let(:create_objects) do
-      @user1 = User.create(name: 'Tom', photo: 'https://picsum.photos/100', bio: 'Teacher from Mexico.',
-                           posts_counter: 0)
-      @post1 = Post.create(author: @user1, title: 'first', text: 'This is my first post', comments_counter: 0,
-                           likes_counter: 0)
-      @post2 = Post.create(author: @user1, title: 'second', text: 'This is my second post', comments_counter: 0,
-                           likes_counter: 0)
-      @post3 = Post.create(author: @user1, title: 'third', text: 'This is my third post', comments_counter: 0,
-                           likes_counter: 0)
-      @post4 = Post.create(author: @user1, title: 'fourth', text: 'This is my fourth post', comments_counter: 0,
-                           likes_counter: 0)
-      @post5 = Post.create(author: @user1, title: 'fifth', text: 'This is my fifth post', comments_counter: 0,
-                           likes_counter: 0)
-      @post6 = Post.create(author: @user1, title: 'sixth', text: 'This is my sixth post', comments_counter: 0,
-                           likes_counter: 0)
+    let!(:user) do
+      User.create(
+        name: 'Tom',
+        photo: 'https://picsum.photos/100',
+        bio: 'Teacher from Mexico.',
+        posts_counter: 0
+      )
     end
 
-    before do
-      create_objects
-      visit user_path(@user1.id)
+    let!(:posts) do
+      (1..6).map do |i|
+        Post.create(
+          author: user,
+          title: "#{i.ordinalize} post",
+          text: "This is my #{i.ordinalize} post",
+          comments_counter: 0,
+          likes_counter: 0
+        )
+      end
     end
+
+    before { visit user_path(user.id) }
 
     scenario "When I click a user's post, it redirects me to that post's show page" do
-      post_element = find('a', text: 'sixth')
-      post_element.click
+      find('a', text: '6th').click
 
-      expect(page).to have_current_path(user_post_path(@post6.author_id, @post6.id))
+      expect(page).to have_current_path(user_post_path(posts[-1].author_id, posts[-1].id))
+    end
+
+    scenario "When I click to see all posts, it redirects me to the user's post's index page" do
+      find('button', text: 'See all posts').click
+
+      expect(page).to have_current_path(user_posts_path(posts[-1].author_id))
     end
   end
 end
