70f8aa2c9570ce8639dddb55e8b3d69aaee08191
Esteban <cepalacios@outlook.com>
2024-01-11 09:48:37 -0500

    Implement before_validation so posts counter is 0 when user is created

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,15 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
+  before_validation :set_default_posts_counter, on: :create
+
   def last_three_posts
     posts.order('created_at DESC').limit(3)
+  end
+
+  private
+
+  def set_default_posts_counter
+    self.posts_counter ||= 0
   end
 end
