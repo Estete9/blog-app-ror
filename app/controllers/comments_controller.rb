ba2a04f1106ae44863beb17f56c33d493d36936e
Esteban <cepalacios@outlook.com>
2024-01-12 17:25:42 -0500

    remove format.json

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -17,7 +17,7 @@ class CommentsController < ApplicationController
     end
     respond_to do |format|
       format.html { redirect_to user_post_path(@user.id, @post.id) }
-      format.json { render json: @comment, status: @comment.save ? :created : :unprocessable_entity }
+      # format.json here
     end
   end
 
