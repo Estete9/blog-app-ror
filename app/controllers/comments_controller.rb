48834fb5d872ec5161421bbcc33451b47200cb41
Esteban <cepalacios@outlook.com>
2024-01-12 23:03:14 -0500

    Fix rubocop linters

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -16,7 +16,7 @@ class CommentsController < ApplicationController
       flash[:error] = "An error occurred when saving your comment: #{error_messages(@comment)}"
     end
 
-    respond_to  do |format|
+    respond_to do |format|
       format.html { redirect_to user_post_path(@user.id, @post.id) }
       format.json { render json: @comment, status: @comment.save ? :created : :unprocessable_entity }
     end
