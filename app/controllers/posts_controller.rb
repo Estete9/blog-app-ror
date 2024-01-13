375831c0d0995746d02e0a4c2f90bd483ad8fac7
Jose RM <mojica.raul@gmail.com>
2024-01-12 21:35:44 -0600

    add response in json for posts index action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,10 @@ class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
     @posts = @user.posts.includes(:author, comments: [:user])
+    respond_to do |format|
+      format.html
+      format.json { render :json => @posts }
+    end
   end
 
   def show
